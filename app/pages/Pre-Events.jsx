9a76b547f845e4925f2860c6f4e5dd863270d46b
rohit27-2 <gaddamrohitgoud@gmail.com>
2024-07-15 00:14:11 +0530

    slider added

--- app/pages/Pre-Events.jsx
+++ app/pages/Pre-Events.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function Gallery() {
   return (
-    <div className='bg-gray-200 rounded-lg md:p-9 sm:p-5  '>
+    <div className=' rounded-lg md:p-9 sm:p-5  '>
       <h2 class="px- font-extrabold text-3xl md:text-5xl text-gray-900 sm:text-xl">Previous-<span class="text-purple-700"
         >Events</span
         ></h2>
@@ -10,7 +10,7 @@ function Gallery() {
       ">
         <div id="slide1" className="carousel-item relative w-full">
           <img
-            src="https://img.daisyui.com/images/stock/photo-1625726411847-8cbb60cc71e6.jpg"
+            src="https://i.pinimg.com/564x/4e/5a/a1/4e5aa1a59e0be2c62bbb357fa56d29d8.jpg"
             className="w-full" />
           <div className="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform justify-between ">
             <a href="#slide4" className="btn btn-circle">❮</a>
@@ -19,7 +19,7 @@ function Gallery() {
         </div>
         <div id="slide2" className="carousel-item relative w-full">
           <img
-            src="https://img.daisyui.com/images/stock/photo-1609621838510-5ad474b7d25d.jpg"
+            src="https://i.pinimg.com/564x/e5/e8/29/e5e829a5d6aa4f15fc4822bd5db5160b.jpg"
             className="w-full" />
           <div className="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform justify-between">
             <a href="#slide1" className="btn btn-circle">❮</a>
@@ -28,7 +28,7 @@ function Gallery() {
         </div>
         <div id="slide3" className="carousel-item relative w-full">
           <img
-            src="https://img.daisyui.com/images/stock/photo-1414694762283-acccc27bca85.jpg"
+            src="https://i.pinimg.com/564x/e5/e8/29/e5e829a5d6aa4f15fc4822bd5db5160b.jpg"
             className="w-full" />
           <div className="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform justify-between">
             <a href="#slide2" className="btn btn-circle">❮</a>
